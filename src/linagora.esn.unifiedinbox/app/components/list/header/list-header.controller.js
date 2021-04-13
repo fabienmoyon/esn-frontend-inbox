de986127fb0b3c85a2bbf5f316ab358d39478543
Fabien MOYON <fmoyon@linagora.com>
2021-04-13 14:16:40 +0200

    WIP 3 panel

--- src/linagora.esn.unifiedinbox/app/components/list/header/list-header.controller.js
+++ src/linagora.esn.unifiedinbox/app/components/list/header/list-header.controller.js
@@ -4,8 +4,11 @@ const _ = require('lodash');
 
 angular.module('linagora.esn.unifiedinbox')
 
-  .controller('inboxListHeaderController', function($scope, esnDatetimeService, inboxFilteringService, INBOX_EVENTS, INBOX_SEARCH_DEBOUNCE_DELAY) {
-    var self = this;
+  .controller('inboxListHeaderController', function($scope, $stateParams, esnDatetimeService, inboxFilteringService, inboxPlugins, INBOX_EVENTS, INBOX_SEARCH_DEBOUNCE_DELAY) {
+    var self = this,
+    account = $stateParams.account,
+    context = $stateParams.context,
+    plugin = inboxPlugins.get($stateParams.type);
 
     self.$onInit = initQuickFilter;
     self.$onChanges = $onChanges;
@@ -17,6 +20,14 @@ angular.module('linagora.esn.unifiedinbox')
 
     function initQuickFilter() {
       self.quickFilter = inboxFilteringService.getQuickFilter();
+
+      if (plugin) {
+        plugin.contextSupportsAttachments(account, context).then(function(value) {
+          self.contextSupportsAttachments = value;
+        });
+      } else {
+        self.contextSupportsAttachments = true;
+      }
     }
 
     function $onChanges(bindings) {
